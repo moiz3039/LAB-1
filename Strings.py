168c2d811269fe24e14995502e2568644c170762
moiz3039 <moiza3039@gmail.com>
2025-02-10 03:12:22 +0500

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,18 @@
+A="Football"
+
+# A String with Literal is stored under a Variable
+
+# STRING METHODS:
+
+print(A.capitalize())
+
+print(A.lower())
+
+print(A.upper())
+
+print(A.swapcase())
+
+print('Quantity of l:', A.count('l'))
+
+print(A.startswith('B'))
+
