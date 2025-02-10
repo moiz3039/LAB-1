1735173764f217223cc39bfeb05d8a8b99d9253d
moiz3039 <moiza3039@gmail.com>
2025-02-10 05:52:20 +0500

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,24 @@
+                                # Question 1 # Dictionaries
+
+
+ex_dict={'m':60,
+             'a':50,
+             'n':40
+             }
+print("Dictionary: ",ex_dict)
+
+print("Keys: ",ex_dict.keys())
+print("Values: ",ex_dict.values())
+
+print(ex_dict.get('a'))
+ex_dict['e']=45
+print("After Adding: ",ex_dict)
+remove_value=ex_dict.pop('m')
+print("After Popping: ",ex_dict)
+ex_dict.update({'a':5,'m':6})
+print("After Updating: ",ex_dict)
+ex_dict.clear()
+print("After Clearing: ",ex_dict)
+
+                                                    # Question 2)
+
